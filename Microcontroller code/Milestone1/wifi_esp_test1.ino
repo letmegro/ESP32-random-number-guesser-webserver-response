e8091692d28581b396a4539a60d61255646a2032
Nicolas Korsunski <Nick.kor23@gmail.com>
2025-02-04 23:17:26 -0500

    Update wifi_esp_test1.ino
    Minot updates for the HTML portion

--- Microcontroller code/Milestone1/wifi_esp_test1.ino
+++ Microcontroller code/Milestone1/wifi_esp_test1.ino
@@ -1,6 +1,6 @@
 #include <WiFi.h>
-const char* ssid = "xxxxxxxx";
-const char* password = "xxxxxxxx";
+const char* ssid = "Cinnabon";
+const char* password = "4hoursLong:)";
 WiFiServer server(80);
 String header = "";
 void setup() {
@@ -60,7 +60,7 @@ void loop(){
             client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
             client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
             client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".container {display:grid; grid-template-columns: auto auto auto; padding: 2px;}");
+            client.println(".container {display: grid; grid-template-columns: 100px 100px 100px; padding: 2px; justify-self: center;}");
             client.println(".container > div {text-align: center; font-size:30px; border: 1px solid black; background-color: #f1f1f1; padding: 10px;}");
             client.println(".button2 {background-color: #555555;}</style></head>");
             //request handling
@@ -73,6 +73,12 @@ void loop(){
             client.println("<div><a href=\"2\">2</a></div>");
             client.println("<div><a href=\"3\">3</a></div>");
             client.println("<div><a href=\"4\">4</a></div>");
+            client.println("<div><a href=\"5\">5</a></div>");
+            client.println("<div><a href=\"6\">6</a></div>");
+            client.println("<div><a href=\"7\">7</a></div>");
+            client.println("<div><a href=\"8\">8</a></div>");
+            client.println("<div><a href=\"9\">9</a></div>");
+            client.println("<div class=\"\"><div><a href=\"0\">0</a></div></div>");
             client.println("</div>");   
             client.println("<p>" + response + "</p>");
             client.println("</body></html>");
