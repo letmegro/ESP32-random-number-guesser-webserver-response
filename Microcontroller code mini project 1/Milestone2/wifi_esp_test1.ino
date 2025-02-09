7336a64415b6d6762f26756b4e641aaec4a075a3
Nicolas Korsunski <Nick.kor23@gmail.com>
2025-02-08 22:28:08 -0500

    Finished off mini project 1
    Added a a random gen value and fixed minor issues

---
+++ Microcontroller code mini project 1/Milestone2/wifi_esp_test1.ino
@@ -0,0 +1,113 @@
+#include <WiFi.h>
+const char* ssid = "xxxx";
+const char* password = "xxxx";
+WiFiServer server(80);
+String header = "";
+void setup() {
+  Serial.begin(115200);
+  Serial.print("Connecting to ");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+}
+int ansVal = genVal();
+bool reGen = false;
+String response = "N/A";
+void loop(){
+  WiFiClient client = server.available();
+  
+  if (client) {
+    Serial.println("New Client.");          
+    String currentLine = "";           
+    while (client.connected()) {            
+      if (client.available()) {             
+        char c = client.read();             
+        Serial.write(c);                    
+        header += c;
+        if (c == '\n') {
+          if(header.indexOf("GET /") >= 0){
+              try{
+                int userVal = header.substring(header.indexOf('/')+1, header.indexOf('H')-1).toInt();
+                if (userVal == ansVal){
+                  response = "Correct!";
+                  reGen = true;
+                }
+                else{
+                  response = "Try again!";
+                  Serial.println(ansVal);
+                }
+              }
+              catch(...){
+                Serial.print("Failed to convert to Integer.");
+              }
+          }
+          else{
+            response = "Please select a number.";
+          }
+          if (currentLine.length() == 0) {
+            //header response
+            client.println("HTTPS/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            Serial.println(header);
+            // header doc
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".container {display: grid; grid-template-columns: 100px 100px 100px; padding: 2px; justify-self: center;}");
+            client.println(".container > div {text-align: center; font-size:30px; border: 1px solid black; background-color: #f1f1f1; padding: 10px;}");
+            client.println(".button2 {background-color: #555555;}</style></head>");
+            //request handling
+            
+            //body
+            client.println("<body><h1>Guess the value</h1>");
+
+            client.println("<div class=\"container\">");
+            client.println("<div><a href=\"1\">1</a></div>");
+            client.println("<div><a href=\"2\">2</a></div>");
+            client.println("<div><a href=\"3\">3</a></div>");
+            client.println("<div><a href=\"4\">4</a></div>");
+            client.println("<div><a href=\"5\">5</a></div>");
+            client.println("<div><a href=\"6\">6</a></div>");
+            client.println("<div><a href=\"7\">7</a></div>");
+            client.println("<div><a href=\"8\">8</a></div>");
+            client.println("<div><a href=\"9\">9</a></div>");
+            client.println("<div class=\"\"><div><a href=\"0\">0</a></div></div>");
+            client.println("</div>");   
+            client.println("<p>" + response + "</p>");
+            client.println("</body></html>");
+            client.println();
+            break;
+          } else {
+            currentLine = "";
+          }
+        } else if (c != '\r') {
+          currentLine += c;      
+        }
+      }
+    }
+    header = "";
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
+  if (reGen == true){
+    ansVal = genVal();
+    reGen = false;
+  }
+}
+
+int genVal(){
+  return ansVal = rand() % 10;
+}
